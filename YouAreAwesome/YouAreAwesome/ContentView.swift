4a0c9770e8e5356371944997d3f1c03dc0c59d9a
Steven E. Collins <iamstevenecollins@gmail.com>
2022-12-01 18:29:11 -0500

    Chapter 1.8

--- YouAreAwesome/YouAreAwesome/ContentView.swift
+++ YouAreAwesome/YouAreAwesome/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var messageString = ""
+    @State private var messageString = "Namaste"
     
     var body: some View {
         ZStack {
             VStack {
                 
                 Spacer()
+                
+                Image(systemName: "cloud.sun.rain.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .symbolRenderingMode(.multicolor)
+                    .padding()
+                    .background(Color(hue: 0.558, saturation: 0.075, brightness: 1.0))
+                    .cornerRadius(30)
+                    .shadow(color: .gray, radius: 30, x:20, y: 20)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 30)
+                            .stroke(.blue, lineWidth: 1)
+                    )
+                    .padding()
                 
                 Text(messageString)
                     .font(.largeTitle)
@@ -25,20 +39,10 @@ struct ContentView: View {
                     .foregroundColor(.red)
                     .frame(height: 150)
                     .frame(maxWidth: .infinity)
-                //                .border(.orange, width: 1)
                     .padding()
-                //                    .background(Color(.gray).opacity(0.15))
                 
                 Spacer()
-                
-                Divider()
-                    .background(.black)
-                    .padding()
-                    .frame(width: 150.0)
-                
-                Rectangle()
-                    .fill(.indigo)
-                
+                                
                 HStack {
                     Button("Awesome") {
                         // This is the action performed when the button is pressed
