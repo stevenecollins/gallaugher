6203e0c29e6506d119732ed6a2eb993c200ca1b3
Steven E. Collins <iamstevenecollins@gmail.com>
2022-12-03 11:23:01 -0500

    Chapter 1.10

--- YouAreAwesome/YouAreAwesome/ContentView.swift
+++ YouAreAwesome/YouAreAwesome/ContentView.swift
@@ -10,10 +10,22 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var messageString = ""
+    @State private var imageName = ""
+    @State private var imageNumber = 0
+    
+    let image0 = "image0"
+    let image1 = "image1"
     
     var body: some View {
         ZStack {
             VStack {
+                
+                Image(imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(30)
+                    .shadow(radius: 30)
+                    .padding()
                 
                 Spacer()
                 
@@ -30,15 +42,23 @@ struct ContentView: View {
                 Spacer()
                 
                 
-                Button("Awesome") {
+                Button("Show Message") {
                     let message1 = "You are Great"
                     let message2 = "You are Awesome"
-                   
-                    if messageString == message1 {
-                        messageString = message2
-                    } else {
-                        messageString = message1
+                    
+                    // Ternary Operator
+                    messageString = ( messageString == message1 ? message2 : message1)
+
+                    imageName = "image\(imageNumber)"
+                    imageNumber = imageNumber + 1
+                    // imageNumber += 1 // Alternative way
+                    if imageNumber > 9 {
+                        imageNumber = 0
                     }
+                    print(imageNumber)
+                    
+                    
+                    
                 }
                 .buttonStyle(.borderedProminent)
             }
